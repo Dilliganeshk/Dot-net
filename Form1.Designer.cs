4430dc0821cc4e667c8b8a91621d8b7857f58ecf
Dilliganeshk <74553604+Dilliganeshk@users.noreply.github.com>
2021-08-06 13:45:59 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 19);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(560, 421);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Firstname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn percentage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Enroll;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source = LAPTOP-8VT8NLKN;Database = dilliganesh;Integrated security=true");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conn.Open();
+            SqlCommandcmd = new SqlCommand("insert into Application('" + textBox1.Text + "','" + textBox2.Text + '",'" + textbox3.Text + '", '" + textBox4.Text + '")", con);
+            Cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Added");
+
+
+        }
+
+              
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows) // foreach datagridview selected rows values  
+                {
+                    lblID.Text = row.Cells[0].Value.ToString();
+                    txtFirstname.Text = row.Cells[0].Value.ToString();
+                    txtLastname.Text = row.Cells[1].Value.ToString();
+                    txtPercentage.Text = row.Cells[2].Value.ToString();
+                    txtEnrollmentno.Text = row cells[3].value.To.String();
+                }
+    }
+}
+
+       
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Selected Item.to String() = "gender")
+                {
+                combobox2.Items.Add clear();
+                combobox2.Items.Add("male");
+                combobox2.Items.Add("female");
+            }
+        }
